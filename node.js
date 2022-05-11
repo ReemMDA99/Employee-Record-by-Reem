5a9e6bae80121e72efbf9853afeb0a9282b1d6c6
Reem Ahmed <reem.mda0909@gmail.com>
2022-05-11 13:22:58 -0600

    start server

--- node.js
+++ node.js
@@ -1,3 +1,18 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+const PORT = process.env.PORT || 3002;
+
+// Not Found response for unmatched routes
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  // Start server after DB connection
+  db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
