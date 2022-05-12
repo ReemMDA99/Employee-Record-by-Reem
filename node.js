91ef58d72f9a1d6582fa90c612edede231d39164
Reem Ahmed <reem.mda0909@gmail.com>
2022-05-12 12:54:28 -0600

    create prompts

--- node.js
+++ node.js
@@ -14,6 +14,35 @@ const db = mysql.createConnection(
     password: 'Career@uoft2022',
     database: 'employeeTracker_db'
   },
-  console.log(`Connected to the employeeTracker_db database.`)
+  console.log(`Successfully connected to the employeeTracker_db database.`)
 );
+// Create prompts for user to select options from menu
+function Menu() {
+  // =====
+  // Menu
+  // =====
+  inquirer.
+    prompt ({
+      type: 'list',
+      name:'menu',
+      message:'Welcome to our EMPLOYEE database, Please select your preferred options from the given Menu:',
+      choices:[
+            "View all departments", 
+            "View all roles", 
+            "View all employees", 
+            "Add a department",
+            "Add a role", 
+            "Add an employee",
+            "Update an employee role",
+            //Add Bonus functionality
+            "Update employee managers",
+            "View employees by manager",
+            "View employees by department",
+            "Delete departments, roles, and employees",
+            "View the total utilized budget of a department",
+            "Quit"
+    ]
+  })
 
+  .then 
+}
