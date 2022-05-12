6cc6a1aa17ccab44c00f7444ec4e1f446d38a8d0
Reem Ahmed <reem.mda0909@gmail.com>
2022-05-12 12:11:11 -0600

    check connections and mysql queries

--- node.js
+++ node.js
@@ -1,19 +1,19 @@
-const db = require('./db/connection');
+//Add dependencies
+//Add inquirer package to interact with the user via the command line
 const inquirer = require('inquirer');
-const consoleTable = require('console.table');
-c
-const PORT = process.env.PORT || 3002;
-const app = express();
-// Not Found response for unmatched routes
-app.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  // Start server after DB connection
-  db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  });
+const mysql = require('mysql2');
+// Add  console.table package to print MySQL rows to the console.
+//npm i console.table
+const cTable = require('console.table');
+
+// create the connection to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Career@uoft2022',
+    database: 'employeeTracker_db'
+  },
+  console.log(`Connected to the employeeTracker_db database.`)
+);
+
